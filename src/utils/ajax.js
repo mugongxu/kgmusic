2ccfea2b93ed2870bf5d56cb2d98909461731edd
unknown <guoqian.xu@jinfuzi.ad>
2018-10-16 18:02:01 +0800

    initapp

---
+++ src/utils/ajax.js
@@ -0,0 +1,25 @@
+/**
+ * 通用AJAX客户端
+ */
+import axios from 'axios';
+
+const ajax = axios.create({
+  timeout: 10000
+});
+
+ajax.interceptors.request.use((config) => {
+  const newConf = { ...config };
+  // 在这里做请求头定制处理
+  return newConf;
+});
+
+ajax.interceptors.response.use((response) => {
+  // 在这里可以对返回做统一错误拦截
+  if (response.status !== 200) {
+    Promise.reject(`Http 状态异常 ${response.status}`);
+  }
+  return response;
+}, (error) => {
+  Promise.reject(error);
+  throw error;
+});
---
+++ src/utils/index.js
@@ -0,0 +1,5 @@
+/**
+ * 公用方法入口
+ */
+export { default as ajax } from './ajax';
+export default {};
