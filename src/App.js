4185e16c29920eb36303e7650490d2aef1d5ae62
unknown <guoqian.xu@jinfuzi.ad>
2018-11-12 18:32:51 +0800

    增加路由

--- src/App.js
+++ src/App.js
@@ -1,6 +1,13 @@
 import React, { Component } from 'react';
 import './App.css';
 import { ajax } from './utils';
+import { BrowserRouter, Route } from 'react-router-dom';
+
+const HomePage = ({ match }) => {
+  return (
+    <div>123456</div>
+  );
+};
 
 class App extends Component {
   constructor(props) {
@@ -11,7 +18,7 @@ class App extends Component {
   }
 
   async tick() {
-    const response = await ajax.get('/plist/list/125032?json=true');
+    const response = await ajax.get('/banner');
     console.log(response);
   }
 
@@ -19,6 +26,12 @@ class App extends Component {
     return (
       <div className="App">
         <a onClick={this.tick.bind(this)}>点击获取信息</a>
+        <BrowserRouter>
+          <div>
+            <Route path="/" exact component={HomePage} />
+            <Route path="/ddd" component={HomePage} />
+          </div>
+        </BrowserRouter>
       </div>
     );
   }
