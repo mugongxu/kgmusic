736b6cae1c7674347c1f7036aa53bb0dae648ea1
unknown <guoqian.xu@jinfuzi.ad>
2019-03-02 16:52:02 +0800

    重新打包

---
+++ build/precache-manifest.b601ecd0d15595fe24419dd28a3d44d8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "858af7b477c4218c51cd",
+    "url": "/static/js/main.858af7b4.chunk.js"
+  },
+  {
+    "revision": "c903de7971f539ed715a",
+    "url": "/static/js/1.c903de79.chunk.js"
+  },
+  {
+    "revision": "858af7b477c4218c51cd",
+    "url": "/static/css/main.a38a2617.chunk.css"
+  },
+  {
+    "revision": "c903de7971f539ed715a",
+    "url": "/static/css/1.7b3f0e3f.chunk.css"
+  },
+  {
+    "revision": "46fdf9696268716d270d9af72efab0a9",
+    "url": "/index.html"
+  }
+];
