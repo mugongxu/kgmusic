526662ff35ede287064e0459c3386356657ad8ec
guoqian.xu <guoqian.xu@jfz.com>
2020-11-24 16:50:15 +0800

    重新打包

---
+++ build/precache-manifest.6ba5fcde9393d94c5209a5c9f917489d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "843e67d4d75dd4f3202a",
+    "url": "/static/css/main.425aa7fb.chunk.css"
+  },
+  {
+    "revision": "843e67d4d75dd4f3202a",
+    "url": "/static/js/main.843e67d4.chunk.js"
+  },
+  {
+    "revision": "d423f44b25ff1de6acf5",
+    "url": "/static/css/1.ee7cf713.chunk.css"
+  },
+  {
+    "revision": "d423f44b25ff1de6acf5",
+    "url": "/static/js/1.d423f44b.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "df5c6a3ca5a3ce2016ea4e6db051f487",
+    "url": "/index.html"
+  }
+];
--- build/service-worker.js
+++ build/service-worker.js
@@ -11,10 +11,10 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.2/workbox-sw.js");
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.081ae79ff4a146ed439859cd037cd267.js"
+  "/precache-manifest.6ba5fcde9393d94c5209a5c9f917489d.js"
 );
 
 workbox.clientsClaim();
@@ -30,5 +30,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute("/index.html", {
   
-  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
